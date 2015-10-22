ae49e9597101f6ed2255c22bca023661ad6cf5fc
rschatz <rschatz@ea.com>
2015-10-22 12:07:05 +0200

    added readme, Start actually uses a generated class

---
+++ README.md
@@ -0,0 +1,3 @@
+Scrooge SBT Scala 2.11
+======================
+Isolates some compiler issue for 2.11
--- src/main/scala/me/Start.scala
+++ src/main/scala/me/Start.scala
@@ -1,7 +1,11 @@
 package me
 
+import me.common.generated.thrift.User
+import me.service.generated.thrift.GetStatsRequest
+
 object Start {
   def main(args: Array[String]) {
+    val request = GetStatsRequest(User("john", "doe"))
     println("Works")
   }
 }
